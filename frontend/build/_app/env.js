67f70873167efe41256efebac80628f450f1f7b5
karlitoyo <karltimmins@gmail.com>
2025-09-30 20:07:43 +0100

    updated site key and seret key for google recaptcha.

--- frontend/build/_app/env.js
+++ frontend/build/_app/env.js
@@ -1,1 +1,1 @@
-export const env={"PUBLIC_RECAPTCHA_SITE_KEY":"6LfiNtkrAAAAAN7vTJGni1av210sT9PgNTsrzsHQ","PUBLIC_DEBUG_LOGS":"1","PUBLIC_APP_ENV":"development","PUBLIC_API_URL":"http://localhost:3000","PUBLIC_FORM_API_URL":"/api/interest/submit/"}
+export const env={"PUBLIC_RECAPTCHA_SITE_KEY":"6LcLOtorAAAAAFsKdjae_4P9cdkkRIZbKfYcrztL","PUBLIC_DEBUG_LOGS":"1","PUBLIC_APP_ENV":"development","PUBLIC_API_URL":"http://localhost:3000","PUBLIC_FORM_API_URL":"/api/interest/submit/"}
--- frontend/build/_app/immutable/entry/start.Cod2QCQW.js
+++
@@ -1,1 +0,0 @@
-import{l as o,b as r}from"../chunks/BA7hE4uB.js";export{o as load_css,r as start};
