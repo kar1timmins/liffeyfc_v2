238a3114e28e213ae5b555a735b5bf7c50c8424c
karlitoyo <karltimmins@gmail.com>
2025-10-13 18:48:55 +0100

    updated - No more overlap between FAB menu and reCAPTCHA badge - Clean user experience when opening the FAB -Both elements remain functional and accessible

---
+++ frontend/build/_app/immutable/entry/start.DtRKX2HM.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/B5wEGQUa.js";import{z as t}from"../chunks/B4GhetWX.js";export{t as load_css,r as start};
--- frontend/build/_app/immutable/entry/start.lc02hxZ0.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/p6CmnOPf.js";import{z as t}from"../chunks/DGMl9Mmy.js";export{t as load_css,r as start};
