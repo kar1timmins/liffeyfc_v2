ecc5877a3de3104d72a4b9aef2db06a229483c53
karlitoyo <karltimmins@gmail.com>
2025-10-16 23:39:57 +0100

    recaptcha size increase for the logo and  build.

---
+++ frontend/build/_app/immutable/entry/start.dd8UsZ-K.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CJo9XRH6.js";import{z as t}from"../chunks/LnHXjtYJ.js";export{t as load_css,r as start};
--- frontend/build/_app/immutable/entry/start.sRZnkbuu.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BpuT9Y9c.js";import{z as t}from"../chunks/DRJW8TH4.js";export{t as load_css,r as start};
