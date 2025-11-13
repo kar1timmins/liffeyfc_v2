0b48ed0be9454dbf673291f33173dec57aa9b1a0
karlitoyo <karltimmins@gmail.com>
2025-11-13 19:29:18 +0000

    updated google sso endpoint updated to backend and build.

--- frontend/build/_app/immutable/entry/start.BbfQ9NVX.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/BiPPkL7P.js";import{y as t}from"../chunks/C7krCyem.js";export{t as load_css,r as start};
---
+++ frontend/build/_app/immutable/entry/start.DlInq1Pz.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/DHo_faA7.js";import{y as t}from"../chunks/B4R-Xf9J.js";export{t as load_css,r as start};
