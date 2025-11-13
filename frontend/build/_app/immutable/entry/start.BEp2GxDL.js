a001488b58b988ef7f17d0b61d62489f0569bc30
karlitoyo <karltimmins@gmail.com>
2025-11-13 20:23:55 +0000

    updated pitch page and build.

--- frontend/build/_app/immutable/entry/start.BEp2GxDL.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/DJOqSti3.js";import{y as t}from"../chunks/CZP-kQWn.js";export{t as load_css,r as start};
---
+++ frontend/build/_app/immutable/entry/start.PAnGwKej.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/DQHVTjzp.js";import{y as t}from"../chunks/BZ-8f9oe.js";export{t as load_css,r as start};
