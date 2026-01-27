c7a37c53887a66f7f4f18418af44bce1585f6978
karlitoyo <karltimmins@gmail.com>
2026-01-27 12:53:08 +0000

    build for updated footer luma event.

---
+++ frontend/build/_app/immutable/entry/start.BTAOXOOS.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/B32Q1f7l.js";import{y as t}from"../chunks/CQ1nOSXy.js";export{t as load_css,r as start};
--- frontend/build/_app/immutable/entry/start.DwT5jWNK.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BEKTguPb.js";import{y as t}from"../chunks/MrxoNT3G.js";export{t as load_css,r as start};
