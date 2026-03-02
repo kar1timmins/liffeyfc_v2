3a91b2ffb14c6f318e1bc9b343d4c8888446fdd7
karlitoyo <karltimmins@gmail.com>
2026-03-02 18:28:48 +0000

    Added frame-src to static .htaccess file for stript crypto onboarding and build.

--- frontend/build/_app/immutable/entry/start.BYSi8ZR_.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CESzBBJd.js";import{y as t}from"../chunks/Dk5JT5XH.js";export{t as load_css,r as start};
---
+++ frontend/build/_app/immutable/entry/start.BhbYi9qU.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/p2-MP_si.js";import{y as t}from"../chunks/DBUI9YlY.js";export{t as load_css,r as start};
