19fbe4715c4d9468e9a8b535cb50220ed791a77e
karlitoyo <karltimmins@gmail.com>
2026-03-11 23:56:29 +0000

    Fix dashboard button text colours (dark:text-white on spans), expired bounty badge, leaderboard count, reset-failed-deployment endpoint - and build.

---
+++ frontend/build/_app/immutable/entry/start.B_CH357s.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/C_MKvpW1.js";import{y as t}from"../chunks/DBJFOgR8.js";export{t as load_css,r as start};
--- frontend/build/_app/immutable/entry/start.Cw9fbMER.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/Yrjwwacx.js";import{y as t}from"../chunks/Bs4ALNMc.js";export{t as load_css,r as start};
